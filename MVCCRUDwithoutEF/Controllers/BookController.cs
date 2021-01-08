44783779056533ad21ff9c052f93294f4c8369ae
Arthur Ryoo <jene0523@gmail.com>
2021-01-07 23:05:07 -0800

    Edit bolierplate

---
+++ MVCCRUDwithoutEF/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUDwithoutEF.Data;
+using MVCCRUDwithoutEF.Models;
+
+namespace MVCCRUDwithoutEF.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly MVCCRUDwithoutEFContext _context;
+
+        public BookController()
+        {
+
+        }
+
+        // GET: Book
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Book/Edit/5
+        public IActionResult AddOrEdit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddOrEdit(int id, [Bind("BookID,Title,Author,Price")] BookViewModel bookViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: Book/Delete/5
+        public IActionResult Delete(int? id)
+        {
+
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
